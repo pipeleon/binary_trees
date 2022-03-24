0c70ea46814d85d57f2792c487b39a148264a340
pipeleon <oflg94@gmail.com>
2022-03-23 23:57:26 -0500

    Task 7 and 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,7 +2,7 @@
 /* Task 6 - 0x1D. C - Binary trees */
 
 /**
- * binary_tree_inorder - Goes through a binary tree using  in-order traversalcp
+ * binary_tree_inorder - Goes through a binary tree using  in-order traversal
  * @tree: Binary tree to go trough
  * @func: Funtion to use
  *
@@ -16,7 +16,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	}
 
 	
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
