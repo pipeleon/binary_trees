c5c38f8be44aa9868b2d89324af0f016bdca6069
pipeleon <oflg94@gmail.com>
2022-03-24 10:44:29 -0500

    Task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,65 @@
+#include "binary_trees.h"
+/* Task 14 - 0x1D. C - Binary trees */
+
+/**
+ * max_value - max value
+ * @a: First
+ * @b: Second
+ *
+ * Return: max value
+ */
+size_t max_value(size_t a, size_t b)
+{
+	if (a > b)
+	{
+		return (a);
+	}
+	else
+	{
+		return (b);
+	}
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: Binary tree to go trough
+ *
+ * Return: height of the node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		return (0);
+	}
+	height = 1
+		+ max_value(binary_tree_height(tree->left), binary_tree_height(tree->right));
+
+	return (height);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: Binary tree to go trough
+ *
+ * Return: count
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int factor;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	factor = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+
+	return (factor);
+}
