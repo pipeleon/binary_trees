4307703dfd16bcfc4747e9964bbb34aec5c90478
pipeleon <oflg94@gmail.com>
2022-03-24 00:05:11 -0500

    Task 8 corrected

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,7 +2,7 @@
 /* Task 6 - 0x1D. C - Binary trees */
 
 /**
- * binary_tree_postorder - Goes through a binary tree using post-order traversal
+ * binary_tree_postorder - Goes through a binarytree using post-order traversal
  * @tree: Binary tree to go trough
  * @func: Funtion to use
  *
@@ -15,7 +15,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	}
 
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
